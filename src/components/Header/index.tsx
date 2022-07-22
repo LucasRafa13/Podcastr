9bf38c377a2739c39c50dd7b913bc703bdf33c67
Lucas <lucasrafael123araujolima@gmail.com>
2022-07-22 03:08:17 -0400

    feat: :sparkles: Início do Projeto

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header>
+      <img src="/logo.svg" alt="Podcastr"></img>
+
+      <p>O melhor para você ouvir, sempre</p>
+
+      <span>Qui, 8 abril</span>
+    </header>
+  )
+}
